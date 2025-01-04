c5ae0309e985c57a71e50e915296f884bf418cd9
Sairaj Mote <motesairaj79@gmail.com>
2025-01-04 19:21:37 +0530

    hello this is trail

--- e-commerce/src/components/Header/index.js
+++ e-commerce/src/components/Header/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import Logo from '../../asets/images/logo.jpg'
 const header = () => {
   return (
-    <div>
+    <>
       <div className="header">
         <div className="topstrip">
-          <div className="container"><p className="mb-0 mt-3 text-center">Welcome to <b>Famazone</b>, we hope u like our services!</p></div>
+          <div className="container-xxl"><p className="mb-0 mt-3 text-center bg-blue text-white ">Welcome to <b>Famazone</b>, we hope u like our services!</p></div>
+        </div>
+        <div className="header1">
+          <div className="row">
+            <div className="Logo container-sm-2"> 
+              <Link to={'/'}><img src={Logo} alt='Famazone' /></Link>
+            </div>
+            <div className="container-sm-9"></div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
